6135a2abacaeb3b6ade6141b6cbaadb084f87c67
zhangyuh000 <zhangyuh000@warren.k12.in.us>
2017-02-15 10:41:14 -0500

    Initial Commit

--- src/Door.java
+++ src/Door.java
@@ -16,6 +16,10 @@ public class Door extends Entity{
         //Color doorColor = new Color (153, 76, 0); CUSTOM COLOR IS BEING A BUTTHOLE
     }
 
+    public boolean collides(Entity other){
+
+    }
+
     public void paint(Graphics g){
         g.setColor(this.getColor());
         g.fillRect(this.getX(), this.getY(), this.getWidth(), this.getHeight());
